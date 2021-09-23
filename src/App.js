b657d72e564366e770317383499cb159212d03b1
Yosr Naija <yosr.naija@live.com>
2021-09-23 15:39:25 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./counter";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Counter</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/counter/counter-value.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+
+const CounterValue = ({count}) => {
+  return <div className="counter-value">{count}</div>
+}
+
+export default CounterValue
